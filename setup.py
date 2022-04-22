9cf2775fbe10e52addc936947044d6a3fb3c4a77
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-04-22 20:24:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+with open('README.md') as readme:
+    long_description = readme.read()
+
 setup(
     name='chull',
     version='0.2.0',
     py_modules=['chull'],
     install_requires=['scipy', 'numpy'],
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    description='convex hull from points',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
